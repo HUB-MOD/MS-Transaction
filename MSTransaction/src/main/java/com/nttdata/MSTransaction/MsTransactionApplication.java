cc848db9c840144dbb4d79b9b9f670747f5d156a
Heber Sixto Uraccahua Barrios <huraccahuab@gmail.com>
2022-03-09 21:29:43 -0500

    fix pom.xml

--- MSTransaction/src/main/java/com/nttdata/MSTransaction/MsTransactionApplication.java
+++ MSTransaction/src/main/java/com/nttdata/MSTransaction/MsTransactionApplication.java
@@ -1,13 +1,30 @@
 package com.nttdata.MSTransaction;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-public class MsTransactionApplication {
+public class MsTransactionApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsTransactionApplication.class, args);
+	}
+
+	private static final Logger logger = LogManager.getLogger(MsTransactionApplication.class);
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		logger.debug("Debug de la aplicación");
+		logger.info("Info Log");
+		logger.warn("Warning");
+		logger.error("Error");
+		logger.fatal("Fatal Error");
 	}
 
 }
